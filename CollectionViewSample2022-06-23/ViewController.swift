9dce1cb3fd697c7a5f5c0c67a4502a5d28aabeec
muranakar <p1211065@gmail.com>
2022-06-23 05:21:02 +0900

    Initial Commit

---
+++ CollectionViewSample2022-06-23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewSample2022-06-23
+//
+//  Created by 村中令 on 2022/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
